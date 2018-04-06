10b94273eebd90aac8cc3a1cc8a167741464bbba
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-04-06 13:39:35 +1000

    Base player and button
    Added the playground
    Ported the tutorial assets
    Gave the player a hand and reach
    Two objects that can be converted to buttons. Currently destroy self on click.

---
+++ Cycle 2/Assets/zPlayground/TestScripts/Press.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press : MonoBehaviour {
+
+    int click = 0;
+    public GameObject hand;
+    public GameObject reach;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if(click == 0 && Input.GetMouseButton(0))
+        {
+            RaycastHit press;
+            if (Physics.Raycast(hand.transform.position, -(hand.transform.position - reach.transform.position).normalized, out press, 1.0f))
+            {
+                if(press.transform.tag == "Button")
+                {
+                    press.transform.GetComponent<killCube>().kill();
+                }
+            }
+        }
+		
+	}
+}
