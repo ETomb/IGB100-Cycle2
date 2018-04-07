145ba90d870a758aacdab7c9ac7c2205b708483a
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-04-07 18:17:23 +1000

    No real changes, for reasons
    Github isn't letting me pull origin without doing this because of me playing around with stuff. No real changes.

--- Cycle 2/Assets/zPlayground/TestScripts/killCube.cs
+++ Cycle 2/Assets/zPlayground/TestScripts/killCube.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class killCube : MonoBehaviour {
 
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
 	}
 
     public void kill()
