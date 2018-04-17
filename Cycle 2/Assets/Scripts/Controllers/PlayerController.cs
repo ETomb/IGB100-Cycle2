093e77b41886506bc3bf5460f0517cc568ec3430
M Kingsley <matt.kingsley@gmail.com>
2018-04-17 22:40:44 +1000

    Progress on updating Player scripts
    Adds new PlayerMotor and PlayerController scripts that currently are WIP and unused.

--- Cycle 2/Assets/Scripts/Controllers/PlayerController.cs
+++ Cycle 2/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,16 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float encumSpeed = 2.5f;
+    [SerializeField] float xSensitivity = 2f;
+    [SerializeField] float ySensitivity = 2f;
+
+    PlayerMotor motor;
+    float speed;
+
+    private void Start() {
+        // Find PlayerMotor
+        motor = GetComponent<PlayerMotor>();
+        // Set initial speed
+        speed = walkSpeed;
+    }
+
+    private void Update() {
+        // Calculate movement velocity as a 3d vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(_velocity);
+
+
+        /// Character Rotation
+        // Calculate rotation as a Quaternion
+        float _yRotation = Input.GetAxisRaw("Mouse X") * xSensitivity;
+
+        Quaternion _rotation = Quaternion.Euler(0f, _yRotation, 0f);
+
+        // Apply rotation
+        motor.Rotate(_rotation);
+
+        /// Camera Rotation
+        // Calculate rotation as a Quaternion
+        float _xRotation = Input.GetAxisRaw("Mouse Y") * ySensitivity;
+
+        Quaternion _cameraRotation = Quaternion.Euler(-_xRotation, 0f, 0f);
+
+        // Apply camera rotation
+        motor.RotateCamera(_cameraRotation);
+
+    }
+
 }
---
+++ Cycle 2/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField] Camera _camera;
+
+    Vector3 velocity = Vector3.zero;
+    Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
+    Quaternion cameraRotation = Quaternion.Euler(0f, 0f, 0f);
+
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    // Gets a movement vector
+    public void Move(Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    // Gets character rotation
+    public void Rotate(Quaternion _rotation) {
+        rotation = _rotation;
+    }
+
+    // Gets camera rotation
+    public void RotateCamera(Quaternion _cameraRotation) {
+        cameraRotation = _cameraRotation;
+    }
+
+    // Perform movement based on velocity vector
+    private void PerformMovement() {
+        if(velocity != Vector3.zero){
+            rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    // Perform rotation
+    private void PerformRotation() {
+        rb.MoveRotation(rb.rotation * rotation);
+        if(_camera != null) {
+            _camera.transform.rotation = cameraRotation;
+        }
+    }
+}
