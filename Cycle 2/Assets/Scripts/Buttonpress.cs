200eba249f6e8b3b97dc393d93086b5f3814b26f
FlowerUrchin <37795082+FlowerUrchin@users.noreply.github.com>
2018-04-23 10:17:03 +1000

    Button Problems

---
+++ Cycle 2/Assets/Scripts/Buttonpress.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonpress : Interactable {
+
+    public int red = 1;
+    public override void Interact()
+    {
+        base.Interact();
+        red = 2;
+        Deactivate();
+    }
+
+    public override void Tooltip()
+    {
+        base.Tooltip();
+    }
+}
